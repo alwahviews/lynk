9c4685a46f576f7b04f60ab07619492b9b21a01a
AllWahViews <alwahviews@gmail.com>
2026-01-19 14:46:00 +0700

    my-lynk-profile

---
+++ main.js
@@ -0,0 +1,7 @@
+// const endpoint = "https://reqres.in/api/users/3";
+
+// fetch(endpoint)
+//   .then((data) => data.json())
+//   .then((result) => console.log(result));
+
+// console.log("wahhhhhhhhhhhhhhhhhhh");
